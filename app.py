c7a1a852ff3b52c3559b7cfe4b4768f1d183d368
Lulubay89 <karolkruczek29@gmail.com>
2025-11-16 12:29:50 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,16 @@
-from flask import Flask, request
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 import os
 
 TOKEN = os.getenv("BOT_TOKEN")
 
-app = Flask(__name__)
-
-telegram_app = Application.builder().token(TOKEN).build()
-
-@app.get("/")
-def home():
-    return "Bot działa! 😊"
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Cześć! Bot jest online i działa poprawnie! 😊")
 
-telegram_app.add_handler(CommandHandler("start", start))
+def start_bot():
+    application = Application.builder().token(TOKEN).build()
 
-@app.post("/webhook")
-def webhook():
-    data = request.get_json(force=True)
-    update = Update.de_json(data, telegram_app.bot)
-    telegram_app.update_queue.put_nowait(update)
-    return "OK"
+    application.add_handler(CommandHandler("start", start))
 
-if __name__ == "__main__":
-    telegram_app.run_polling()
+    print("Bot wystartował!")
+    application.run_polling()
